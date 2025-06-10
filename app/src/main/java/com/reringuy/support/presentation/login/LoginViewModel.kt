7a651346cfdf18f32b7c0e1b8e60d1ba2b1e2bf9
joao <joao.projex@gmail.com>
2025-06-10 18:20:24 -0400

    Updated Gradle dependencies and removed logs
    Updated AGP to 8.10.1, lifecycleRuntimeKtx to 2.9.1, composeBom to 2025.06.00, and appcompat to 1.7.1.
    Removed debug logs from LoginViewModel.

--- app/src/main/java/com/reringuy/support/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/reringuy/support/presentation/login/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.reringuy.support.presentation.login
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.reringuy.support.auth.TokenManager
 import com.reringuy.support.helper.BaseViewModel
@@ -42,10 +41,8 @@ class LoginViewModel @Inject constructor(
         viewModelScope.launch {
             authRepository.login(auth).collect {
                 if (it == null) {
-                    Log.d("ble", "deu erro")
                     sendEvent(LoginEvents.LoadUser(OperationHandler.Error("Usuario nao encontrato.")))
                 } else {
-                    Log.d("ble", "n deu erro")
                     tokenManager.saveToken(it.token)
                     tokenManager.saveUser(it.user)
                     sendEvent(LoginEvents.LoadUser(OperationHandler.Success(it.user)))
